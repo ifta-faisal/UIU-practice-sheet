cdcc482706c7cbf570b085327c64a16a7de62826
Ifta Faisal <iftafaisal759@gmail.com>
2022-12-13 20:44:47 -0800

    q3.2

--- s3p3,1.c
+++ s3p3,1.c
@@ -1,12 +1,18 @@
 #include<stdio.h>
+#include<conio.h>
 int main()
 {
-int i,n;
-printf("Enter the value");
-scanf("%d",&n);
-for(i=1;i<=n;i++)
-{
-    printf("%d\n",i);
-}
-
+    int i, n;
+    printf("Enter the value of N (limit): ");
+    scanf("%d", &N);
+    printf("\n");
+    for(i=1; i<=n; i++)
+    {
+    if(i==N)
+            printf("%d", i);
+    else
+     printf("%d,", i);
+    }
+getch();
+    return 0;
 }
